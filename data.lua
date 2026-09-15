3467271d91ccc6d66ca31196383dd406285ef2f1
UnknownRori <unknownrori@proton.me>
2026-09-15 19:40:44 +0700

    prep for 0.1.0 release

--- data.lua
+++ data.lua
@@ -1,6 +1,7 @@
 local fish = data.raw["capsule"]["raw-fish"]
 
 -- fascinating isn't, just simple change of graphics
-fish.localised_name = "Wakasagihime"
+fish.localised_name = { "entity-name.raw-fish" }
+fish.localised_description = { "item-description.raw-fish" }
 fish.icon = "__th-wakasagihime__/graphics/wakasagihime-icon.png"
-fish.icon_size = 32
+fish.icon_size = 84 -- I know it weird but it's because my drawing okay
