a3db88860885500a5465afb203ae2d8dae6ab0cc
UnknownRori <unknownrori@proton.me>
2026-09-16 11:00:55 +0700

    scaffold: for 0.2.0 release based on current tasks at hand

--- data.lua
+++ data.lua
@@ -5,3 +5,14 @@ fish.localised_name = { "entity-name.raw-fish" }
 fish.localised_description = { "item-description.raw-fish" }
 fish.icon = "__th-wakasagihime__/graphics/wakasagihime-icon.png"
 fish.icon_size = 84 -- I know it weird but it's because my drawing okay
+
+-- space age dlc recipe specific
+if data.raw["recipe"]["fish-breeding"] then
+	data.raw["recipe"]["fish-breeding"].icon = "__th-wakasagihime__/graphics/wakasagihime-breeding.png"
+	data.raw["recipe"]["fish-breeding"].icon_size = 64
+end
+
+if data.raw["recipe"]["nutrients-from-fish"] then
+	data.raw["recipe"]["nutrients-from-fish"].icon = "__th-wakasagihime__/graphics/nutrients-from-wakasagihime.png"
+	data.raw["recipe"]["nutrients-from-fish"].icon_size = 64
+end
